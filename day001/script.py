8ea7ce8da8d1ea0ae529a8b06f931ff45ad41704
Phillip Bridgeman <phillipbridgeman@gmail.com>
2025-09-08 21:07:55 -0500

    Update script to day01. Finished!

--- day001/script.py
+++ day001/script.py
@@ -1,5 +1,8 @@
 '''
 Day 1: 100 Days of Python
-Basics of Python Programming Language
+Description: Creating a brand name from combining city with pet names to create the brand name.
 '''
-print("Hello World")
+print("Welcome to the Brand Name Generator.")
+city = input("What's the name of city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
+print(f"Your Brand Name could be {city} {pet_name}")
